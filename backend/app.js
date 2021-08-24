97a8daf04ce489c2bb5d8ec259f8b982b335b5b6
FranMori <francois.moriame@gmail.com>
2021-08-24 15:13:49 +0200

    Etape 2 : Parcours utilisateur

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,34 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const User = require ('./models/user')
+const userRoutes = require('./routes/user')
+
+
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 mongoose.connect('mongodb+srv://Dazak:openclassrooms@cluster0.8sae2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  console.log('Requête reçue')
-  next()
-})
 
-app.use((req, res, next) => {
-  res.status(201)
-  next()
-})
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue'})
-  next()
-})
-
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès')
-})
+app.use('/api/auth', userRoutes)
 
 module.exports = app
+
