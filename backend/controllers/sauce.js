c2828ed8e626f63464b39ffa70dc0faa5fbb2304
FranMori <francois.moriame@gmail.com>
2021-09-03 09:43:56 +0200

    Ajout route put et delete

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,16 +1,11 @@
 const Sauce = require('../models/sauce')
+const fs = require('fs')
 
 exports.createSauce =  (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce)
-  delete sauceObject._id
   const sauce = new Sauce ({
-    // ...sauceObject,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-    mainPepper: req.body.mainPepper,
-    heat: req.body.heat
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   })
   sauce.save()
     .then(() => res.status(201).json({message:'Sauce enregistrée'}))
@@ -19,15 +14,28 @@ exports.createSauce =  (req, res, next) => {
 
 
 exports.modifySauce =  (req,res,next) => {
-  Sauce.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body}
+  Sauce.updateOne({ _id: req.params.id}, {...sauceObject, _id: req.params.id})
     .then(() =>res.status(200).json({message:'Sauce modifiée'}))
     .catch(error => res.satatus(400).json({error}))
 }
 
 exports.deleteSauce =  (req,res,next) => {
-  Sauce.deleteOne({ _id: req.params.id})
+  Sauce.findOne({ _id: req.params.id})
+  .then(sauce => {
+    const filename = sauce.imageUrl.split('/images/')[1]
+    fs.unlink(`images/${filename}`, () => {
+      Sauce.deleteOne({ _id: req.params.id})
     .then(() => res.status(200).json({message: 'Sauce supprimée'}))
     .catch(error => res.status(400).json({error}))
+    })
+  })
+  .catch(error => res.satus(500).JSON({error}))
+  
 }
 
 exports.getOneSauce =  (req,res,next) => {
